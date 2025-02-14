ccdfc5240b099c2fe4acec65f84b2bcc0cca4e32
JonathanDeGeyter <jonathan.degeyter1996@gmail.com>
2025-02-14 16:57:03 +0100

    Add files via upload

---
+++ Evalutatie1.ino
@@ -0,0 +1,49 @@
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-lm35-temperature-sensor
+ */
+#include <BH1750.h>
+#include <Wire.h>
+#define ADC_VREF_mV    3300.0 // in millivolt
+#define ADC_RESOLUTION 4096.0
+#define PIN_LM35       33 // ESP32 pin GPIO36 (ADC0) connected to LM35
+BH1750 lightMeter;
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(D1, D2);
+  lightMeter.begin();
+  Serial.println(F("BH1750 Test begin"));
+
+  // set the ADC attenuation to 11 dB (up to ~3.3V input)
+  analogSetAttenuation(ADC_11db);
+}
+
+void loop() {
+  // read the ADC value from the temperature sensor
+  int adcVal = analogRead(PIN_LM35);
+  // convert the ADC value to voltage in millivolt
+  float milliVolt = adcVal * (ADC_VREF_mV / ADC_RESOLUTION);
+  // convert the voltage to the temperature in °C
+  float tempC = milliVolt / 10;
+  // convert the °C to °F
+  float tempF = tempC * 9 / 5 + 32;
+
+  // print the temperature in the Serial Monitor:
+  Serial.print("Temperature: ");
+  Serial.print(tempC);   // print the temperature in °C
+  Serial.print("°C");
+  Serial.print("  ~  "); // separator between °C and °F
+  Serial.print(tempF);   // print the temperature in °F
+  Serial.println("°F");
+
+  delay(1000);
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+  delay(1000);
+
+}
